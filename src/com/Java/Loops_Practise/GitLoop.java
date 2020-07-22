62fe4cd5399692882bdf2d2a119c9f3990ecd964
Sasank <sasank.theeda@gmail.com>
2020-07-22 19:25:15 +0100

    QA UK

---
+++ src/com/Java/Loops_Practise/GitLoop.java
@@ -0,0 +1,17 @@
+package com.Java.Loops_Practise;
+
+public class GitLoop {
+
+	public static void loopIt(int num) {
+		for (int i = 1; i <num; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		loopIt(51);
+
+	}
+
+}
